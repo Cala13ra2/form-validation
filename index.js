f27f7bb0501d597ce7580e29548c7f334c5e427a
claire <143410666+Cala13ra2@users.noreply.github.com>
2023-10-19 02:32:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("her")
+
+const handleForm = (e)=>{
+    e.preventDefault()
+    const email = document.getElementById("i1").value
+    const password = document.getElementById("i2").value
+
+    const errorOne = document.getElementById("errorOnee")
+    const error = document.getElementById("errorTwoo")
+    if(!email){
+        errorOne.textContent = "Please enter an email"
+    }
+    if(!password){
+        error.textContent = "Please enter your password"
+    }
+
+    console.log
+}
+button.addEventListener("click",handleForm)
+
